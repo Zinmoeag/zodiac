0ff1f26ec93fc03895e590bcf097d072324a9751
Ast_Zinmoag <astro9857@gmail.com>
2023-10-27 20:43:03 +0630

    add deploy workflow

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  // Your Vite configuration options go here
+  // For example:
+  // build: {
+  //   outDir: 'dist', // Specify the output directory for production builds
+  // },
+  // plugins: [/* add any plugins you need */],
+  // ...other configuration options
+
+  base : "https://zinmoeag.gitlab.io/zodiac/"
+}
